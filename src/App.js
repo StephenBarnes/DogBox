8a2037327c6c3e5cf5f1e37e746a18552607d08d
J <stbarnes@stanford.edu>
2021-04-25 16:18:47 -0500

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 import './App.css';
 import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listFiles } from './graphql/queries';
 import { createFile as createFileMutation, deleteFile as deleteFileMutation } from './graphql/mutations';
+
 
 function App() {
   const [files, setFiles] = useState([]);
@@ -50,30 +59,66 @@ function App() {
     const signedUrl = await Storage.get(name);
     window.open(signedUrl, '_blank').focus();
   }
+  
 
   return (
-    <div className="App">
-      <h1>DogBox</h1>
-      <input
-        type="file"
-        ref={fileInputRef}
-        onChange={uploadFile}
-      />
-      <div style={{marginBottom: 30}}>
-        {
-          files.map(file => (
-            <div key={file.name}>
-              <h3>{file.name}</h3>
-              <p>{bytesToString(file.bytes)}</p>
-              <p>Uploaded {file.createdAt}</p>
-              <button onClick={() => downloadFile(file)}>Download</button>
-              <button onClick={() => deleteFile(file)}>Delete</button>
+    <React.Fragment>
+      <CssBaseline />
+      <main>
+        <div className="header">
+          <Container>
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              <img src="doge.png" width="100px" alt="doge" />
+              DogBox
+            </Typography>
+            <div>
+              <Grid container justify="center">
+                <Grid item>
+                  <Button variant="contained" component="label" color="primary" className="upload-button">
+                    Upload<input type="file" hidden ref={fileInputRef} onChange={uploadFile} />
+                  </Button>
+                </Grid>
+              </Grid>
             </div>
-          ))
-        }
+          </Container>
+        </div>
+        <Container className="card-grid" maxWidth="md">
+          <Grid container spacing={4}>
+            {files.map((file) => (
+              <Grid item key={file.name} xs={12} sm={6} md={4} lg={3}>
+                <Card className="card">
+                  <CardContent className="card-content">
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {file.name}
+                    </Typography>
+                    <Typography>
+                      {bytesToString(file.bytes)}
+                    </Typography>
+                    <Typography variant="body2">
+                      {file.createdAt.split('T')[0]}
+                    </Typography>
+                    <Typography variant="body2">
+                      {file.createdAt.split('T')[1]}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary" onClick={() => downloadFile(file)}>
+                      View
+                    </Button>
+                    <Button size="small" color="primary" onClick={() => deleteFile(file)}>
+                      Delete
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      <div id="amplify-sign-out">
+        <AmplifySignOut />
       </div>
-      <AmplifySignOut />
-    </div>
+    </React.Fragment>
   );
 }
 
